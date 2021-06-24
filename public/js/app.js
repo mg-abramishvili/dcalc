b8fd14e9aa04e0c6faa10f94af90a50e45d8468e
Mikhail Abramishvili <mg@abramishvili.net>
2021-06-24 17:14:06 +0500

    .

--- public/js/app.js
+++ public/js/app.js
@@ -57645,54 +57645,8 @@ var render = function() {
         ),
         _vm._v(" "),
         _c("li", { staticClass: "sidebar-header" }, [
-          _vm._v("\n\t\t\t\t\tЗадачи и пользователи\n\t\t\t\t")
+          _vm._v("\n\t\t\t\t\tПользователи\n\t\t\t\t")
         ]),
-        _vm._v(" "),
-        _c(
-          "li",
-          { staticClass: "sidebar-item" },
-          [
-            _c(
-              "router-link",
-              { staticClass: "sidebar-link", attrs: { to: "/tasks" } },
-              [
-                _c(
-                  "svg",
-                  {
-                    staticClass: "feather feather-check-square align-middle",
-                    attrs: {
-                      xmlns: "http://www.w3.org/2000/svg",
-                      width: "24",
-                      height: "24",
-                      viewBox: "0 0 24 24",
-                      fill: "none",
-                      stroke: "currentColor",
-                      "stroke-width": "2",
-                      "stroke-linecap": "round",
-                      "stroke-linejoin": "round"
-                    }
-                  },
-                  [
-                    _c("polyline", { attrs: { points: "9 11 12 14 22 4" } }),
-                    _c("path", {
-                      attrs: {
-                        d:
-                          "M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"
-                      }
-                    })
-                  ]
-                ),
-                _vm._v(" "),
-                _c("span", { staticClass: "align-middle" }, [_vm._v("Задачи")]),
-                _vm._v(" "),
-                _c("span", { staticClass: "badge badge-sidebar-primary" }, [
-                  _vm._v("1")
-                ])
-              ]
-            )
-          ],
-          1
-        ),
         _vm._v(" "),
         _c(
           "li",
