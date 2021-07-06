726f02acd2470c2fe9d6a29ef9bdb0b7fa12c452
Mikhail Abramishvili <mg@abramishvili.net>
2021-07-06 14:45:35 +0500

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .postCss('resources/css/app.css', 'public/css');
+    .postCss('resources/css/app.css', 'public/css').version();
