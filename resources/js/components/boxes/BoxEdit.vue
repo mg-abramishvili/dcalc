38f64b9ff1db7314ac354b838260ad1704939061
Mikhail Abramishvili <mg@abramishvili.net>
2021-07-09 00:14:03 +0500

    .

--- resources/js/components/boxes/BoxEdit.vue
+++ resources/js/components/boxes/BoxEdit.vue
@@ -110,7 +110,7 @@
                     this.pre_usd = response.data.pre_usd,
                     this.marzha = response.data.marzha,
                     this.sborka = response.data.sborka,
-                    this.price = parseInt(response.data.price),
+                    this.price = response.data.price,
                     this.selected_types = response.data.types.map(type => type.id),
                     this.width = response.data.width,
                     this.height = response.data.height,
@@ -130,6 +130,7 @@
                 .then(response => (
                     this.types = response.data
                 ));
+            this.TotalPrice()
         },
         methods: {
             onChange(index, event) {
